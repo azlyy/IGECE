d81eb9eb6e181792eebeedb15c649a540a0f62fa
azlyy <117789464+azlyy@users.noreply.github.com>
2022-11-27 22:42:20 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
-P = '\x1b[1;97m'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
 try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nKONEKSI INTERNET BURUK\n'%(P))
-	exit()
-
-if __name__ == "__main__":
-	if "Indonesia" == fc:
-		__import__("igc").login_kamu()
-	else:
-		__import__("igc").login_kamu()
+	from igc import login_kamu()
+except:
+	os.system('python .igc.py')
+	
