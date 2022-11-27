62317ad58180a8452a13f7ab8aa34a7da2eb55d1
azlyy <117789464+azlyy@users.noreply.github.com>
2022-11-28 00:23:24 +0700

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import os, platform
-from os import path,system
-from platform import uname
-arch=uname().machine.lower()
-if 'aarch' in arch:
-	import igc
-	igc.login_kamu()
-else:
-	os.system('python .igc.py')
