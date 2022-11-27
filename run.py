1b9684f43a4b8a338958b01dc4a6571afc5cf7ab
azlyy <117789464+azlyy@users.noreply.github.com>
2022-11-27 23:03:39 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-try:
-	from igc import login_kamu()
-except:
+import os, platform
+from os import path,system
+from platform import uname
+arch=uname().machine.lower()
+if 'aarch' in arch:
+	import igc
+	igc.login_kamu()
+else:
 	os.system('python .igc.py')
-	
